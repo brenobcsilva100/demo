f6fe368c176335b0441e7e7fcdf4cd09e055b15c
Breno <brenobcsilva@gmail.com>
2025-07-04 04:57:03 -0300

    get e set professor

---
+++ src/main/java/com/example/demo/repository/model/Professor.java
@@ -0,0 +1,35 @@
+package com.example.demo.repository.model;
+
+public class Professor {
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String senha;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFirst_name() {
+        return first_name;
+    }
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+    public String getLast_name() {
+        return last_name;
+    }
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
+}
